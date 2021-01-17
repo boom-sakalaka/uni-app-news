25c9a48b35f1de28a988d1459a6ef2cd3b17d04f
Gu <936285702@qq.com>
2021-01-17 22:04:05 +0800

    添加搜索标签

--- pages/home-search/home-search.vue
+++ pages/home-search/home-search.vue
@@ -1,6 +1,19 @@
 <template>
-	<view>
+	<view class="home">
 		<navbar :isSearch="true"></navbar>
+		<view class="home-list">
+			<view class="label-box">
+				<view class="label-header">
+					<text class="label-title">搜索历史</text>
+					<text class="label-clear">清空</text>
+				</view>
+				<view class="label-content">
+					<view v-for="item in 100" class="label-content_item">
+						{{item}} 内容
+					</view>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -17,6 +30,49 @@
 	}
 </script>
 
-<style>
-
+<style lang="scss">
+	page{
+		height: 100%;
+		display: flex;
+		background-color: #f5f5f5;
+	}
+	.home {
+		display: flex;
+		flex-direction: column;
+		flex: 1;
+		.label-box {
+			background-color: #fff;
+			margin-bottm: 10px;
+			.label-header {
+				display: flex;
+				justify-content: space-between;
+				font-size: 14px;
+				color: #666;
+				padding: 10px 15px;
+				border-bottom: 1px #f5f5f5 solid;
+				.label-title {
+					color: $mk-base-color;
+				}
+				.label-clear {
+					color: #30b33a;
+					font-weight: bold;
+				}
+			}
+			.label-content {
+				display: flex;
+				flex-wrap: wrap;
+				padding: 15px;
+				padding-top: 0;
+				.label-content_item {
+					padding: 2px 10px;
+					margin-top: 12px;
+					margin-right: 10px;
+					border-radius: 5px;
+					border: 1px #666 solid;
+					font-size: 14px;
+					color: #666;
+				}
+			}
+		}
+	}
 </style>
